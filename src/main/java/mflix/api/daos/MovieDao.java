2db10cd5c6dccdc5879d691f9c6fc47faa97362e
catalin.oprea <oprea1991@gmail.com>
2020-07-20 00:36:59 +0300

    #Solves get Comments

--- src/main/java/mflix/api/daos/MovieDao.java
+++ src/main/java/mflix/api/daos/MovieDao.java
@@ -11,12 +11,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import static com.mongodb.client.model.Aggregates.*;
 import static com.mongodb.client.model.Filters.all;
+import static com.mongodb.client.model.Filters.expr;
 import static com.mongodb.client.model.Projections.fields;
 import static com.mongodb.client.model.Projections.include;
+import static com.mongodb.client.model.Sorts.descending;
 
 @Component
 public class MovieDao extends AbstractMFlixDao {
@@ -34,9 +38,23 @@ public class MovieDao extends AbstractMFlixDao {
 
     @SuppressWarnings("unchecked")
     private Bson buildLookupStage() {
-        return null;
+        String from = "comments";
+        String as = "comments";
 
+        Variable<String> let = new Variable<String>("id", "$_id");
+
+        Document eq = Document.parse("{'$eq':['$movie_id','$$id']}");
+        Bson match = match( expr ( eq ));
+        Bson sort = sort( descending("date")); //fix from forum
+
+        return lookup(
+                from,                       //from
+                Arrays.asList(let),         //let
+                Arrays.asList(match, sort), //pipeline
+                as                          //as
+        );
     }
+
 
     /**
      * movieId needs to be a hexadecimal string value. Otherwise it won't be possible to translate to
@@ -66,12 +84,16 @@ public class MovieDao extends AbstractMFlixDao {
 
         List<Bson> pipeline = new ArrayList<>();
         // match stage to find movie
-        Bson match = Aggregates.match(Filters.eq("_id", new ObjectId(movieId)));
+        Bson match = match(Filters.eq("_id", new ObjectId(movieId)));
+        Bson joinWithComments = buildLookupStage();
+        //System.out.println ("match aaa " + match);
         pipeline.add(match);
-        // TODO> Ticket: Get Comments - implement the lookup stage that allows the comments to
-        // retrieved with Movies.
-        Document movie = moviesCollection.aggregate(pipeline).first();
+        //System.out.println ("joinWithComments  aaa" + joinWithComments);
 
+        pipeline.add(joinWithComments);
+
+        Document movie = moviesCollection.aggregate(pipeline).first();
+       // System.out.println ("movie  aaa" +movie);
         return movie;
     }
 
@@ -87,7 +109,7 @@ public class MovieDao extends AbstractMFlixDao {
     public List<Document> getMovies(int limit, int skip) {
         String defaultSortKey = "tomatoes.viewer.numReviews";
         List<Document> movies =
-                new ArrayList<>(getMovies(limit, skip, Sorts.descending(defaultSortKey)));
+                new ArrayList<>(getMovies(limit, skip, descending(defaultSortKey)));
         return movies;
     }
 
@@ -169,7 +191,7 @@ public class MovieDao extends AbstractMFlixDao {
      */
     public List<Document> getMoviesByCast(String sortKey, int limit, int skip, String... cast) {
         Bson castFilter = Filters.in("cast", cast);
-        Bson sort = Sorts.descending(sortKey);
+        Bson sort = descending(sortKey);
         //TODO> Ticket: Subfield Text Search - implement the expected cast
         // filter and sort
         // was done
@@ -197,7 +219,7 @@ public class MovieDao extends AbstractMFlixDao {
         // query filter
         Bson castFilter = Filters.in("genres", genres);
         // sort key
-        Bson sort = Sorts.descending(sortKey);
+        Bson sort = descending(sortKey);
         List<Document> movies = new ArrayList<>();
         // TODO > Ticket: Paging - implement the necessary cursor methods to support simple
         // pagination like skip and limit in the code below
@@ -271,8 +293,8 @@ public class MovieDao extends AbstractMFlixDao {
         List<Document> movies = new ArrayList<>();
         String sortKey = "tomatoes.viewer.numReviews";
         Bson skipStage = Aggregates.skip(skip);
-        Bson matchStage = Aggregates.match(Filters.in("cast", cast));
-        Bson sortStage = Aggregates.sort(Sorts.descending(sortKey));
+        Bson matchStage = match(Filters.in("cast", cast));
+        Bson sortStage = sort( descending( sortKey));
         Bson limitStage = Aggregates.limit(limit);
         Bson facetStage = buildFacetStage();
         // Using a LinkedList to ensure insertion order
